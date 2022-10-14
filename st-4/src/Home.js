553d8c6488e2f45443d7438d515ddd8295934ffd
gurbakshishsingh14 <75017241+gurbakshishsingh14@users.noreply.github.com>
2022-10-14 22:34:29 +0530

    Add files via upload

---
+++ st-4/src/Home.js
@@ -0,0 +1,101 @@
+import React, { useState, useEffect } from "react";
+// import tech from "./tech.jpeg";
+// import tech2 from "./tech2.jpeg";
+// import tech3 from "./tech3.jpeg";
+// import ds from './ds.jpeg'
+// import wd from './wd.jpeg'
+// import sales from './sales.jpeg'
+import './Home.css';
+const Home = () => {
+  return (
+    <React.Fragment>
+      {/* <h1 classNameName="text-center text-danger text-capitalize my-4">Carrier In Tech Companies</h1>
+    <div classNameName="container ">
+    <div classNameName="row my-1">
+  <div classNameName="col-sm">
+    <div classNameName="card" > */}
+    {/* <div className="bg-image hover-zoom"> */}
+    {/* <a href="https://en.wikipedia.org/wiki/Computer_security" target="blank"> */}
+  {/* <img src={tech} alt="..." height="250px" classNameName="card-img-top" /> */}
+  {/* </a> */}
+  {/* <a src="https://en.wikipedia.org/wiki/Computer_security"> */}
+  {/* <div className="mask" style="background-color: rgba(57, 192, 237, 0.2)"></div> */}
+  {/* </a> */}
+  {/* </div> */}
+  {/* <div classNameName="card-body">
+    <h5 classNameName="card-title">Cyber Security</h5>
+    <p classNameName="card-text">Cyber security is the practice of defending computers, servers, mobile devices, electronic systems, networks, and data from malicious attacks.</p>
+    <a href="https://en.wikipedia.org/wiki/Computer_security" target="blank" classNameName="btn btn-primary">Open</a>
+  </div>
+</div></div>
+  <div classNameName="col-sm"><div classNameName="card"> */}
+    {/* <a href="https://en.wikipedia.org/wiki/Artificial_intelligence" target="blank"> */}
+      {/* <img src={tech2} classNameName="card-img-top" alt="..." height="250px" /> */}
+    {/* </a> */}
+  {/* <div classNameName="card-body">
+    <h5 classNameName="card-title">Artificial Intelligence</h5>
+    <p classNameName="card-text">Artificial intelligence (AI) is intelligence demonstrated by machines, as opposed to the natural intelligence displayed by animals and humans.</p>
+    <a href="https://en.wikipedia.org/wiki/Artificial_intelligence" target="blank" classNameName="btn btn-primary">Open</a>
+  </div>
+</div></div>
+  <div classNameName="col-sm"><div classNameName="card">
+  <img src={tech3} classNameName="card-img-top" alt="..." height="250px" />
+  <div classNameName="card-body">
+    <h5 classNameName="card-title">Machine Learning</h5>
+    <p classNameName="card-text">Machine learning is a branch of artificial intelligence (AI) and computer science which focuses on the use of data and algorithms.</p>
+    <a href="https://en.wikipedia.org/wiki/Machine_learning" target="blank" classNameName="btn btn-primary">Open</a>
+  </div>
+</div></div> */}
+{/* </div><br/>
+<div classNameName="row">
+<div classNameName="col-sm"><div classNameName="card">
+  <img src={ds} classNameName="card-img-top" alt="..." height="250px" />
+  <div classNameName="card-body">
+    <h5 classNameName="card-title">Data Science</h5>
+    <p classNameName="card-text">Data science is an interdisciplinary field that uses scientific methods, processes, algorithms and systems to extract or extrapolate knowledge and insights from noisy, structured and unstructured data,</p>
+    <a href="https://en.wikipedia.org/wiki/Data_science" target="blank" classNameName="btn btn-primary">Open</a>
+  </div>
+</div></div> */}
+{/* </div> */}
+{/* <div classNameName="row"> */}
+{/* <div classNameName="col-sm"><div classNameName="card">
+  <img src={wd} classNameName="card-img-top" alt="..." height="250px" />
+  <div classNameName="card-body">
+    <h5 classNameName="card-title">Web Developement</h5>
+    <p classNameName="card-text">Web development is the work involved in developing a website for the Internet (World Wide Web) or an intranet (a private network).[</p>
+    <a href="https://en.wikipedia.org/wiki/Web_development" target="blank" classNameName="btn btn-primary">Open</a>
+  </div>
+</div></div>
+<div classNameName="col-sm"><div classNameName="card">
+  <img src={sales} classNameName="card-img-top" alt="..." height="250px" />
+  <div classNameName="card-body">
+    <h5 classNameName="card-title">Sales and Marketing</h5>
+    <p classNameName="card-text">Sales are activities related to selling or the number of goods sold in a given targeted time period. The delivery of a service for a cost is also considered a sale.
+</p>
+    <a href="https://en.wikipedia.org/wiki/Sales" target="blank" classNameName="btn btn-primary">Open</a>
+  </div>
+</div></div>
+</div>
+</div> */}
+
+<div classNameName="header">
+
+<div className="header-content">
+    <div classNameName="header-main-content">
+       <p id="a1">We Offer Courses.</p>
+       <h1 id="a2">Learn With Commune 
+        <br>
+        </br>
+        {/* <span className="clr"></span>
+         Web Deveolpment */}
+      </h1>
+        <a href="#" className="header-btn">Learn More</a>
+    </div>
+ </div>
+
+</div>
+</React.Fragment>
+  );
+};
+
+export default Home;
